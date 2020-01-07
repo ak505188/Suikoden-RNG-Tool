7289f6e886862b4ab21cce5297831aa1a7ec12ee
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2020-01-06 20:47:30 -0800

    Updates

---
+++ precache-manifest.742834c31f796664fa91f445c1ef7a4e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23551533f6bba70a08addd8be755878",
+    "url": "/Suikoden-RNG-Tool/index.html"
+  },
+  {
+    "revision": "46f1feafd8a63884b986",
+    "url": "/Suikoden-RNG-Tool/static/css/2.ba220c4e.chunk.css"
+  },
+  {
+    "revision": "33c8124465be7d8a449c",
+    "url": "/Suikoden-RNG-Tool/static/css/main.03da2e84.chunk.css"
+  },
+  {
+    "revision": "46f1feafd8a63884b986",
+    "url": "/Suikoden-RNG-Tool/static/js/2.e15a1cd9.chunk.js"
+  },
+  {
+    "revision": "69c3a74a68966e6fb58e4430840982c6",
+    "url": "/Suikoden-RNG-Tool/static/js/2.e15a1cd9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "33c8124465be7d8a449c",
+    "url": "/Suikoden-RNG-Tool/static/js/main.e8fcec18.chunk.js"
+  },
+  {
+    "revision": "e641f2fdcb0555b3a643",
+    "url": "/Suikoden-RNG-Tool/static/js/runtime-main.b3e45268.js"
+  },
+  {
+    "revision": "61a7efea562c6fbb52e7e3b34fb820ff",
+    "url": "/Suikoden-RNG-Tool/static/media/GoldenHydraLeft.61a7efea.png"
+  },
+  {
+    "revision": "d4e4014ecf78534145b88c0896ce5783",
+    "url": "/Suikoden-RNG-Tool/static/media/GoldenHydraRight.d4e4014e.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Suikoden-RNG-Tool/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Suikoden-RNG-Tool/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Suikoden-RNG-Tool/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Suikoden-RNG-Tool/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Suikoden-RNG-Tool/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/Suikoden-RNG-Tool/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Suikoden-RNG-Tool/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Suikoden-RNG-Tool/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Suikoden-RNG-Tool/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Suikoden-RNG-Tool/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Suikoden-RNG-Tool/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Suikoden-RNG-Tool/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Suikoden-RNG-Tool/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Suikoden-RNG-Tool/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Suikoden-RNG-Tool/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Suikoden-RNG-Tool/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Suikoden-RNG-Tool/precache-manifest.95b168c52376257ec4a4608e731add99.js"
+  "/Suikoden-RNG-Tool/precache-manifest.742834c31f796664fa91f445c1ef7a4e.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Suikoden-RNG-Tool/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Suikoden-RNG-Tool/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
